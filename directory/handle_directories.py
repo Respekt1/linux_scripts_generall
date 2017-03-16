ef49ae3bd33c0d33e45c68aa951d839458f797f1
Respekt1 <Respekt1@users.noreply.github.com>
2017-03-16 15:29:45 +0100

    add recursive_overwrite

--- directory/handle_directories.py
+++ directory/handle_directories.py
@@ -8,3 +8,20 @@ def mkdir( path ):
 		os.makedirs( path, 0755 );
     except OSError:
 	pass
+
+def recursive_overwrite(src, dest, ignore=None):
+    if os.path.isdir(src):
+        if not os.path.isdir(dest):
+            os.makedirs(dest)
+        files = os.listdir(src)
+        if ignore is not None:
+            ignored = ignore(src, files)
+        else:
+            ignored = set()
+        for f in files:
+            if f not in ignored:
+                recursive_overwrite(os.path.join(src, f), 
+                                    os.path.join(dest, f), 
+                                    ignore)
+    else:
+        shutil.copyfile(src, dest)
